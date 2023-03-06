13809e71c8a8d34c3f94f1fcc83ba876b62a1d35
younglee <young.lee@ucb.edu.bo>
2023-03-06 19:31:48 -0400

    cambiando a const idNota

--- src/presenter.js
+++ src/presenter.js
@@ -6,7 +6,7 @@ const form = document.querySelector("#keep-form");
 const div = document.querySelector("#resultado-div");
 const keep = new Keep();
 const modalE = document.querySelector("#modalE");
-idNota="idNota";
+const idNota="idNota";
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   keep.anotar(titulo.value,descripcion.value);
