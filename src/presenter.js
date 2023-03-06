d63f557dda18a9eac1b1a8d84d6714a12f1b90cd
younglee <young.lee@ucb.edu.bo>
2023-03-06 19:33:58 -0400

    reorganizando funciones

--- src/presenter.js
+++ src/presenter.js
@@ -13,6 +13,11 @@ form.addEventListener("submit", (event) => {
   div.innerHTML = keep.getNotasHtml();
 });
 div.addEventListener("click", (event) => {
-  keep.ELiminarNota(event.target.id,"eliminarModal");
+  idNota=event.target.id;
+  window.modalE.showModal();
+});
+modalE.addEventListener("click", (event) => {
+  keep.ELiminarNota(idNota,event.target.id);
+  window.modalE.close();
   div.innerHTML = keep.getNotasHtml();
 });
